ad13e0509610ebfcf9861e042dbedfd974637eb3
Kaviya89 <71933883+Kaviya89@users.noreply.github.com>
2024-08-10 09:40:45 +0530

    Time: 0 ms (100%), Space: 45.6 MB (41.05%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -2,7 +2,6 @@ class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length-1;
-        boolean isAsc = nums[start] < nums[end];
         while(start <= end){
             int mid = start + ((end - start)/2);
             if(nums[mid]==target){
